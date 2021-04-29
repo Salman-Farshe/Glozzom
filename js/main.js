c11854ce315223b71c8225125e099e65200e2fd0
Salman Farshe <salmanfarshe39@gmail.com>
2021-04-29 18:02:21 +0600

    adding video section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,23 @@ $('.carousel').carousel({
     interval: 6000,
     pause: 'hover'
 });
+
+/*
+
+
+// video auto play
+$(function(){
+    $('.video').click(function(){
+        var themodal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC + "https://www.youtube.com/watch?v=V1Pl8CzNzCw&list=PL8hNzTE3OJCi9ajut39NZadf1eUIsr37p&index=2";
+
+        $(themodal + 'iframe').attr('src', videoSRCauto);
+        $(themodal + 'button.close').click(function(){
+            $(themodal + 'iframe').attr('src', videoSRC);
+        });
+    });
+});
+
+
+*/
